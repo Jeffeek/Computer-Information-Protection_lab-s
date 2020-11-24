458835d110087f6b619adf743b67f3d324bcce15
Mikhail Ilyushin <mishamine26@mail.ru>
2020-11-24 10:30:11 +0300

    лабораторная 4. Шифр Виженера

---
+++ CIP_lab_4/Model/WiegenerAlgorithm.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace CIP_lab_4.Model
+{
+    public class WiegenerAlgorithm
+    {
+        private char[] russianAlphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя,.".ToCharArray();
+        private char[] englishAlphabet = "abcdefghijklmnopqrstuvwxyz,.".ToCharArray();
+        private char[] _alphabet;
+
+        public WiegenerAlgorithm(bool isEnglish)
+        {
+            _alphabet = isEnglish ? englishAlphabet : russianAlphabet;
+        }
+
+        public string Encrypt(string message, string key)
+        {
+            message = message.ToLower().Trim();
+            key = key.ToLower().Trim();
+
+            string result = "";
+
+            int keyword_index = 0;
+
+            foreach (char symbol in message)
+            {
+                int c = (Array.IndexOf(_alphabet, symbol) +
+                         Array.IndexOf(_alphabet, key[keyword_index])) % _alphabet.Length;
+
+                result += _alphabet[c];
+
+                keyword_index++;
+
+                if ((keyword_index + 1) == key.Length)
+                    keyword_index = 0;
+            }
+
+            return result;
+        }
+
+        public string Decrypt(string message, string key)
+        {
+            message = message.ToLower().Trim();
+            key = key.ToLower().Trim();
+
+            string result = "";
+
+            int keyword_index = 0;
+
+            foreach (char symbol in message)
+            {
+                int p = (Array.IndexOf(_alphabet, symbol) + _alphabet.Length -
+                         Array.IndexOf(_alphabet, key[keyword_index])) % _alphabet.Length;
+
+                result += _alphabet[p];
+
+                keyword_index++;
+
+                if ((keyword_index + 1) == key.Length)
+                    keyword_index = 0;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ CIP_lab_4/View/WiegenerWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace CIP_lab_4.View
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class WiegenerWindow : Window
+    {
+        public WiegenerWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
